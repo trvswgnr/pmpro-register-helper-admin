3867eb63ad73ae1057785391dc7a8b9a0d309281
trav <trvswgnr@gmail.com>
2020-02-01 18:18:26 -0500

    change plugin uri to git repo

--- pmpro-register-helper-admin.php
+++ pmpro-register-helper-admin.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Paid Memberships Pro - Register Helper Admin
- * Plugin URI: https://travisaw.com
+ * Plugin URI: https://github.com/trvswgnr/pmpro-register-helper-admin.git
  * Description: Add PMPro fields to checkout and user profile from the admin panel (used with Paid Memberships Pro Register Helper Add On).
  * Version: 1.0.0
  * Author: Travis Aaron Wagner
