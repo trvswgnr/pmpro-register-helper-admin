a5e1afa065fb4064013d9ab2f36cf35cd19dc929
trav <trvswgnr@gmail.com>
2020-02-01 22:37:27 -0500

    update version

--- pmpro-register-helper-admin.php
+++ pmpro-register-helper-admin.php
@@ -3,7 +3,7 @@
  * Plugin Name: Paid Memberships Pro - Register Helper Admin
  * Plugin URI: https://github.com/trvswgnr/pmpro-register-helper-admin.git
  * Description: Add PMPro fields to checkout and user profile from the admin panel (used with Paid Memberships Pro Register Helper Add On).
- * Version: 1.1.1
+ * Version: 1.1.3
  * Author: Travis Aaron Wagner
  * Author URI: https://travisaw.com
  * Text Domain: pmprorha
@@ -16,18 +16,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'PMPRORHA_DIR', dirname( __FILE__ ) );
-
-require_once PMPRORHA_DIR . '/classes/class-register-helper-admin.php';
+require_once 'vendor/plugin-update-checker/plugin-update-checker.php';
+require_once 'classes/class-register-helper-admin.php';
+require_once 'classes/class-plugin-update-checker.php';
 
 $pmpro_register_helper_admin = new PMPro\Register_Helper_Admin();
 
-require 'plugin-update-checker/plugin-update-checker.php';
-$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+$update_checker = PMPro\Plugin_Update_Checker::buildUpdateChecker(
 	'https://github.com/trvswgnr/pmpro-register-helper-admin/',
 	__FILE__,
 	'pmpro-register-helper-admin'
 );
 
 // set the branch that contains the stable release.
-$myUpdateChecker->setBranch( 'master' );
+$update_checker->setBranch( 'master' );
+
+$update_checker->getVcsApi()->enableReleaseAssets();
+
+PMPro\Plugin_Update_Checker::auto_check_for_updates();
